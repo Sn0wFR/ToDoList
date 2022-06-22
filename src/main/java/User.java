2c1cb300eec2a053ed51dda1040433e4a6d7a61b
Remy Hamed <remyhamed@MacBook-de-Remy.local>
2022-06-22 12:35:25 +0200

    add checkemail

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,6 +1,7 @@
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.regex.Pattern;
 
 public class User {
     public String mail;
@@ -35,11 +36,19 @@ public class User {
     }
 
 
-    /*
-    public checkEmail(String str) {
 
-      return CustomEmailValidator(str);
-    }*/
+    public Boolean checkEmail(String str) {
+
+        if (str == null || str.isEmpty()) {
+            return false;
+        }
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\." +"[a-zA-Z0-9_+&*-]+)*@" + "(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        Pattern pattern = Pattern.compile(emailRegex);
+        if (pattern.matcher(str).matches()) {
+            return true;
+        }
+        return false;
+    }
 
     public boolean checkDate(Date d) {
 
